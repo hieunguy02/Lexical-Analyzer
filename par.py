abd940687edf553ecc8814b799ee2100884dfd45
hieunguy02 <134108393+hieunguy02@users.noreply.github.com>
2024-03-31 17:09:57 -0700

    trying to get the par.py starting

--- par.py
+++ par.py
@@ -1,18 +1,35 @@
 from lexical import lexer
-from lexical import file_output
 
-'''
-lexer()
+tokens = []
+lexemes = []
+token = ""
+lexeme = ""
+current_i = 0
+find = True
+
+while (current_i < len(tokens)):
+    pass
+
+
+
+
+
+
+
+
+
 
 
 while(find):
   try:
     file_name = input('Enter the filename: ')
+    switch = input("Enter 0(Turn off syntax) or 1(Turn on synxtax): ")
+    lexer(file_name)
     file_output = file_name + "_output.txt"
-    lexer(file_name, file_output)
+    fileoutput = open(file_output, "w")
+    fileoutput.write("Lexemes:           Tokens:" + "\n")
+    for i in range(len(tokens)):     
+      fileoutput.write(str(lexemes[i]) + "            " + str(tokens[i]) + "\n")
     find = False
   except OSError:
      print("Can not find the file.")
-
-'''
-
