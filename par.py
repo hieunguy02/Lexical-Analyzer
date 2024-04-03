3bf45747165d1d73af2fc3df9cefc4ad36fcdcf6
hieunguy02 <134108393+hieunguy02@users.noreply.github.com>
2024-04-02 21:28:31 -0700

    update branch

--- par.py
+++ par.py
@@ -8,37 +8,94 @@ syntax = ""
 token = ""
 lexeme = ""
 
-
-
 current_i = 0
 find = True
 
 #create the function needed from the syntax
 #function taking a current_i
 
+#R1
 def RATS24(current_i, switch):
     if switch == "1":
       fileoutput.write("<Rat24S> ::= $ <Opt Function Definitions> $ <Opt Declaration List> $ <Statement List> $")
       if lexemes[current_i] == "$":
         current_i += 1
         OptFunctionDef(current_i, switch)
-               
+        if lexemes[current_i] == "$":
+            current_i += 1
+            OptDeclarationList(current_i, switch)
+            if lexemes[current_i] == "$":
+                current_i += 1
+                StatementList(current_i, switch)
+                if lexemes[current_i] == "$":
+                    current_i += 1
+                else: fileoutput.write("Syntax Error, expected a $")
+            else: fileoutput.write("Syntax Error, expected a $")
+        else: fileoutput.write("Syntax Error, expected a $")
+    
+    
+#R2               
 def OptFunctionDef(current_i, switch):
-    pass
-         
+    if switch == "1":
+        fileoutput.write("<Opt Function Definitions> ::= <Function Definitions> | <Empty>")
+        FunctionDef(current_i, switch) or Empty()
+        
+#R3        
 def FunctionDef(current_i, switch):
-    pass
-      
+    if switch == "1":
+        fileoutput.write("<Function Definitions> ::= <Function> | <Function> <Function Definitions>")
+        Function(current_i, switch) 
+        FunctionDef(current_i, switch); 
+        
+           
+#R4      
 def Function(current_i, switch):
-    pass
-  
+    if switch == "1":
+        fileoutput.write("<Function> ::= function <Identifier> ( <Opt Parameter List> ) <Opt Declaration List> <Body>")
+        if lexemes == "function":
+            current_i += 1
+            #not finish yet
+            
+            
 def StatementList(current_i, switch):
     pass
 
 def OptDeclarationList(current_i, switch):
     pass
 
-def Empty(current_i, switch):
+#R21
+def Scan(current_i, switch):
+    fileoutput.write("<Scan> ::= scan ( <IDs> );")
+    if lexemes[current_i] == "scan":
+        current_i += 1
+        if lexemes[current_i] == "(":
+            current_i += 1
+            IDS()
+            if lexemes[current_i] == ")":
+                current_i += 1
+                if lexemes[current_i] == ";":
+                    current_i += 1
+                else: fileoutput.write("Syntax Error, Expected ;")
+            else: fileoutput.write("Syntax Error, Expected )")
+        else: fileoutput.write("Syntax Error, Expected ()")
+    
+def IDS():
+    pass
+
+#R24
+def Reloop(current_i, switch):
+    if switch == "1":
+        fileoutput.write("<Relop> ::= == | != | > | < | <= | =>")
+        relop = ["==", "!=", "!=", ">", "<", "<=", "=>"]
+        if lexemes[current_i] in relop:
+            current_i += 1
+        else:
+            fileoutput.write("Syntax Error")
+
+
+
+#R29
+def Empty():
     pass
 
 while (current_i < len(tokens)):
